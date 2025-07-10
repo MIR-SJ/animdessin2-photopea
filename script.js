d398a5c85edf6673611601f3115e31c395dd9c27
MIR-SJ <74208555+MIR-SJ@users.noreply.github.com>
2025-07-10 22:53:57 +0530

    Update script.js
    so my plugin is more like a frame manager and simply does onion skinning without the use of video layers.

--- script.js
+++ script.js
@@ -1,69 +1,80 @@
-let currentIndex = 0;
-let playing = false;
+// Photopea-compatible AnimDessin2 Lite (Layer-based Frame Manager)
+// Add this script to your plugin's script.js
 
-function postToPhotopea(code) {
-  window.parent.postMessage(code, "*");
+let frameCount = 0;
+
+// Helper to run Photopea scripts
+function postToPhotopea(script) {
+  window.parent.postMessage({
+    type: "script",
+    script: script
+  }, "*");
 }
 
+// Add a blank frame (new transparent layer)
 function addFrame() {
-  const jsx = `
+  const script = `
     var doc = app.activeDocument;
-    var layer = doc.artLayers.add();
-    layer.name = "Frame_" + doc.artLayers.length;
-    doc.activeLayer = layer;
+    var newLayer = doc.artLayers.add();
+    newLayer.name = "Frame_" + doc.artLayers.length;
+    doc.activeLayer = newLayer;
+    doc.selection.selectAll();
+    doc.selection.clear();
   `;
-  postToPhotopea(jsx);
+  postToPhotopea(script);
+  frameCount++;
 }
 
+// Duplicate the currently selected frame
 function duplicateFrame() {
-  const jsx = `
+  const script = `
     var doc = app.activeDocument;
-    doc.activeLayer.duplicate();
+    var dup = doc.activeLayer.duplicate();
+    dup.name = "Frame_copy_" + doc.artLayers.length;
+    doc.activeLayer = dup;
   `;
-  postToPhotopea(jsx);
+  postToPhotopea(script);
 }
 
-function prevFrame() {
-  const jsx = `
+// Onion Skinning - lower opacity of layer above and below
+function onionSkin() {
+  const script = `
     var doc = app.activeDocument;
-    var idx = doc.artLayers.getByName(doc.activeLayer.name).itemIndex;
-    if (idx > 0) doc.activeLayer = doc.artLayers[idx - 1];
+    var layers = doc.artLayers;
+    var idx = layers.findIndex(l => l == doc.activeLayer);
+    if (idx > 0) layers[idx - 1].opacity = 30;
+    if (idx < layers.length - 1) layers[idx + 1].opacity = 30;
+    doc.activeLayer.opacity = 100;
   `;
-  postToPhotopea(jsx);
+  postToPhotopea(script);
 }
 
-function nextFrame() {
-  const jsx = `
-    var doc = app.activeDocument;
-    var idx = doc.artLayers.getByName(doc.activeLayer.name).itemIndex;
-    if (idx < doc.artLayers.length - 1) doc.activeLayer = doc.artLayers[idx + 1];
-  `;
-  postToPhotopea(jsx);
+// UI buttons
+function createUI() {
+  const container = document.createElement("div");
+  container.style = "padding: 10px; font-family: sans-serif;";
+
+  const title = document.createElement("h3");
+  title.innerText = "AnimDessin2 Lite";
+
+  const btn1 = document.createElement("button");
+  btn1.innerText = "Add Blank Frame";
+  btn1.onclick = addFrame;
+
+  const btn2 = document.createElement("button");
+  btn2.innerText = "Duplicate Frame";
+  btn2.onclick = duplicateFrame;
+
+  const btn3 = document.createElement("button");
+  btn3.innerText = "Onion Skin Toggle";
+  btn3.onclick = onionSkin;
+
+  [title, btn1, btn2, btn3].forEach(el => {
+    el.style = "display: block; margin-bottom: 10px; width: 150px;";
+    container.appendChild(el);
+  });
+
+  document.body.appendChild(container);
 }
 
-function toggleOnionSkin() {
-  const jsx = `
-    var doc = app.activeDocument;
-    for (var i = 0; i < doc.artLayers.length; i++) {
-      var layer = doc.artLayers[i];
-      layer.opacity = (layer.opacity === 100) ? 20 : 100;
-    }
-  `;
-  postToPhotopea(jsx);
-}
-
-function playAnimation() {
-  if (playing) return;
-  playing = true;
-  const jsx = `
-    var doc = app.activeDocument;
-    for (var i = 0; i < doc.artLayers.length; i++) {
-      doc.artLayers[i].visible = false;
-    }
-    doc.artLayers[0].visible = true;
-  `;
-  postToPhotopea(jsx);
-  setTimeout(() => {
-    playing = false;
-  }, 2000);
-}
+createUI();
